0c7b16f98d8d6a82714c8a2224b93fa3ed7e313f
payalshirsat123 <payalshirsat09@gmail.com>
2025-05-11 22:30:05 +0530

    Update Task1

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-echo "Hello, PHP World!";
+echo "Hello, PHP World...............1\n";
+
+echo "welcome to the APEX PLANET INTERNSHIP this is task 1st where i have created a setup for backend by using PHP and SQL \n";
 ?>
+
